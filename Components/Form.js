4da278b6498dc0be6f6a03096e58da476620bc99
Ananya Gupta <anaya29003@gmail.com>
2023-09-24 23:02:38 +0530

    24-Sept

--- Components/Form.js
+++ Components/Form.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import '@/app/globals.css'
+"use client";
+import React, { useState } from "react";
+import "@/app/globals.css";
 
 const form = () => {
-    return(
-        <>
-        <form className="font-Poppins">
-            <input type="text" className="border-4" placeholder="Heading" />
+  //creating variables for storing title and description
+  const [heading, setHeading] = useState("");
+  const [description, setDescription] = useState("");
 
-            <input type="text" className="border-4" placeholder="Description" />
+  const setHandler = (e)=>{
+    //this basically will prevent reloading the page once we click on add task button
+    e.preventDefault()
+    console.log(heading)
+    console.log(description)
+    setHeading("")
+    setDescription("")
+  }
 
-            <button className="border-4">Add Task</button>
-        </form>
-        </>
-    )
-}
+  return (
+    <>
+      <form className="font-Poppins" onSubmit={setHandler}>
+        <input 
+        type="text" 
+        className="heading" 
+        value={heading}
+        onChange={(event)=>{
+            setHeading(event.target.value)
+        }}
+        placeholder="Heading" />
 
-export default form
+        <input 
+        type="text" 
+        className="description" 
+        value={description}
+        onChange={(event)=>{
+            setDescription(event.target.value)
+        }}
+        placeholder="Description" />
+
+        <button className="btn">Add Task</button>
+      </form>
+    </>
+  );
+};
+
+export default form;
--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ import Form from '@/Components/Form'
 const page = () => {
   return (
     <>
-    <h1 className="text-4xl text-center font-bold font-DancingScript mb-1.5 p-6 shadow-xl bg-indigo-500 text-white">Start your day with some exciting activities </h1>
-    <Form/>
+    <div className="main">
+      <h1 className="text-4xl text-center font-bold font-DancingScript mb-1.5 p-6 shadow-xl text-white">Start your day with some exciting activities </h1>
+      <Form/>
+    </div>
     </>
   );
 }
