6a8f8427b9299705b760ae7fe1bc42540156d90d
Ananya Gupta <anaya29003@gmail.com>
2023-10-05 00:20:09 +0530

    5-Oct

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,27 +1,62 @@
-"use client"
-import React,{ useState } from 'react'
+"use client";
+import React, { useState } from "react";
+import axios from 'axios'
 import "./page.css";
 
 const page = () => {
-    const [name,setName]=useState('');
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-    const [cpassword,setCpassword]=useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
   return (
     <>
-    <h1>Sign Up Form</h1>
-    <form action="POST">
+      <h1>Sign Up Form</h1>
+      <form action="POST">
         <label>Name</label>
-        <input type="text" value={name} placeholder="Enter Your name" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Enter Your name"
+        />
         <label>Email</label>
-        <input type="email" value={email} placeholder="Enter Your email" />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Enter Your email"
+        />
         <label>Password</label>
-        <input type="password" value={password} placeholder="Create a strong password"/>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Create a strong password"
+        />
         <label>Confirm Password</label>
-        <input type="password" value={cpassword} placeholder="Confirm the password" />
-    </form>
-    </>
-  )
-}
+        <input
+          type="password"
+          value={cpassword}
+          onChange={(e) => {
+            setCpassword(e.target.value);
+          }}
+          placeholder="Confirm the password"
+        />
 
-export default page
+        <button type="submit">Create my Account</button>
+      </form>
+    </>
+  );
+};
+
+export default page;
