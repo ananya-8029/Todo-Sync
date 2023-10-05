e51c4e05dcbb8b1921f88a13ca863ef7d6843ff3
Ananya Gupta <anaya29003@gmail.com>
2023-10-05 23:25:32 +0530

    5-Oct

--- app/signup/page.js
+++ app/signup/page.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./page.css";
+import Link from 'next/link'
 
 const page = () => {
   const [name, setName] = useState("");
@@ -12,7 +13,8 @@ const page = () => {
     e.preventDefault();
 
     try {
-      await axios.post("http://localhost:5000/signup", {
+      await axios
+        .post("http://localhost:5000/signup", {
           name,
           email,
           password,
@@ -34,40 +36,52 @@ const page = () => {
   };
   return (
     <>
-      <h1>Sign Up Form</h1>
-      <form action="POST">
-        <label>Name</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          placeholder="Enter Your name"
-        />
-        <label>Email</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          placeholder="Enter Your email"
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          placeholder="Create a strong password"
-        />
+      <img className="logo" src="/images/logo.png" alt="" />
+      <div className="signuppage">
+        <div className="signupform">
+          <h1>Get Started!</h1>
+          <form action="POST">
+            <div className="inputfield nameinputfield">
+              <label>Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="Type in your name"
+              />
+            </div>
+            <div className="inputfield emailinputfield">
+              <label>Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                placeholder="Type in your email"
+              />
+            </div>
+            <div className="inputfield">
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                placeholder="Key your password"
+              />
+            </div>
 
-        <button className="bg-slate-600" onClick={submit} type="submit">
-          Create my Account
-        </button>
-      </form>
+            <button className="signupbtn" onClick={submit} type="submit">
+              Create Account
+            </button>
+          </form>
+          <p>Already have an account? <Link href="/login"><span>Proceed to your account.</span></Link></p>
+        </div>
+      </div>
     </>
   );
 };
