d56b6ebf0b48a620dff22d3573434106d7982c8e
Ananya Gupta <anaya29003@gmail.com>
2024-07-05 22:25:23 +0530

    Update

--- Backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config({ path: ".env.local" });
-const mongoURI = process.env.MONGO_URI;
-
-const connectToMongo = () => {
-  mongoose.connect(mongoURI);
-};
-
-module.exports = connectToMongo;
